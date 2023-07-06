dc634e5f573ace021c7811f2b2e48304515b2686
Curtis <curtis.turk@hl.co.uk>
2023-07-06 13:57:43 +0100

    Main types broken down into structs and enums for options

--- Cookcademy/Recipe.swift
+++ Cookcademy/Recipe.swift
@@ -8,11 +8,41 @@
 import Foundation
 
 struct Recipe {
-  var name: String
-  var description: String
-  var author: String
-  var category: String
-    //Breakfast, Lunch, Dinner, Dessert
-  var ingredients: [String]
-  var directions: [String]
+    var mainInformation: MainInformation
+    var ingredients: [Ingredient]
+    var directions: [Direction]
+}
+
+struct MainInformation {
+    var name: String
+    var description: String
+    var author: String
+    var category: Category
+    
+    enum Category: String, CaseIterable {
+        case breakfast = "Breakfast"
+        case lunch = "Lunch"
+        case dinner = "Dinner"
+        case dessert = "Dessert"
+    }
+}
+
+struct Ingredient {
+    var name: String
+    var quantity: String
+    var unit: Unit
+    
+    enum Unit: String, CaseIterable {
+        case oz = "Ounces"
+        case g = "Grams"
+        case cups = "Cups"
+        case tbs = "Tablespoons"
+        case tsp = "Teaspoons"
+        case none = "No units"
+    }
+}
+
+struct Direction {
+    var description: String
+    var isOptional: Bool
 }
